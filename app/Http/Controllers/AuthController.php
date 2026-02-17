0d152f4f19d442fb66fdfa85bfe1c2ad0ab58261
NBlancs <bnoel.omar@gmail.com>
2026-02-17 21:16:26 +0800

    Current validator added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+
+class AuthController extends Controller
+{
+    //
+
+    public function register(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+        ]);       
+    }
+}
